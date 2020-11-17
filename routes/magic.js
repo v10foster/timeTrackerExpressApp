09896f19fb3cd6c2188c4ff55959750b86848664
Jonathan Foster (ANALOG.PLATFORM) 🚀 <jofoste@microsoft.com>
2020-11-16 17:29:21 -0800

    fix quotes

--- routes/magic.js
+++ routes/magic.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var skillBarText = '<p>HTML</p><div class=/"container/"><div class=/"skills html/">90%</div></div>'
+var skillBarText = '<p>HTML</p><div class="container"><div class="skills html">90%</div></div>'
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {

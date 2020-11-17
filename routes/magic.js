c24b7ec3e26cb14d36d05719df5c9597902c0293
Jonathan Foster (ANALOG.PLATFORM) 🚀 <jofoste@microsoft.com>
2020-11-16 17:25:29 -0800

    adding progress bar to magic

--- routes/magic.js
+++ routes/magic.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+var skillBarText = '<p>HTML</p><div class=/"container/"><div class=/"skills html/">90%</div></div>'
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with magic');
+//  res.send('respond with magic');
+  res.send(skillBarText);
 });
 
 module.exports = router;
